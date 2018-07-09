922f71b4306b290807959562b1d392542a8af00e
wachka06 <wachka06@gmail.com>
2018-07-09 20:12:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  new_array
   array.each do |name|
-    puts "Hello, my name is #{name}."
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
 end 
