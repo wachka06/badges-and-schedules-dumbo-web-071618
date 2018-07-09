754aa17ffbec5c47a9ab58fbef7bc2f3603250c9
wachka06 <wachka06@gmail.com>
2018-07-09 20:22:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,4 +17,5 @@ def assign_rooms(attendees)
   attendees.each_with_index do |name, index|
     new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
   end
+  return new_array
 end
